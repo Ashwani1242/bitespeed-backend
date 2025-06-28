10e97bc45b0dba3dd012c6d7fb0371b83f88565a
Ashwani1242 <ashwanigupta1242@gmail.com>
2025-06-28 20:37:49 +0530

    some fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import identifyRouter from "./routes/identify.router";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.listen(8080, () => {
+app.use(cors());
+
+app.listen(process.env.PORT || 8080, () => {
     console.log("Server is running on port 8080");
 });
 
